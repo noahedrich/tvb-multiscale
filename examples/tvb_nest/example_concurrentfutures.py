85b87264d466e6a2e41877214c92b5e67d101bb3
noahedrich <49396355+noahedrich@users.noreply.github.com>
2022-02-25 13:28:53 +0100

    add example script using concurrent.futures

---
+++ examples/tvb_nest/example_concurrentfutures.py
@@ -0,0 +1,27 @@
+#... here all the imports
+import itertools
+import concurrent
+import numpy as np
+
+# lists of all weights that want to check
+w1 = np.arange(0,1,0.25)
+w2 = np.arange(0,1,0.25)
+w3 = np.arange(0,1,0.25)
+
+# all possible combinations of weights
+a = [w1, w2, w3]
+input_param = list(itertools.product(*a))
+input_param = [np.array(params) for params in input_param]
+print(input_param)
+
+
+# dummy simulation function
+def sim_run(w1, w2, w3):
+    return (w1, w2, w3), w1 + w2 + w3
+
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executor:
+    results = executor.map(sim_run, input_param)
+    search_results = tuple(results)
+
+print(search_results)
