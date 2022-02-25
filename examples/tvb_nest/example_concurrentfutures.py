0c98c9590987db80da926ff0b983b1cadcecad64
noahedrich <49396355+noahedrich@users.noreply.github.com>
2022-02-25 14:54:55 +0100

    fix input parameter list shape

--- examples/tvb_nest/example_concurrentfutures.py
+++ examples/tvb_nest/example_concurrentfutures.py
@@ -11,20 +11,25 @@ w3 = np.arange(0,1,0.25)
 # all possible combinations of weights
 a = [w1, w2, w3]
 input_param = list(itertools.product(*a))
-input_param = [np.array(params) for params in input_param]
+input_param = np.array([np.array(params) for params in input_param])
+input_param = input_param.T
 print('PARAMETERS:')
 print(input_param)
 
 
 # dummy simulation function
 def sim_run(w1, w2, w3):
-    return w1 + w2 + w3
+    out = w1 + w2 + w3
+    print('%.1f + %.1f + %.1f = %.1f' % (w1, w2, w3, out))
+    return out
+
 
 print('\nRESULTS:')
+# run locally, not parallel
+# results = map(sim_run, *input_param)
+# print(list(results))
+
+# run parallel
 with ProcessPoolExecutor(max_workers=10) as executor:
-    for params in input_param:
-        results = executor.map(sim_run, params)
-        print(list(results))
-        print(float(results))
-        print(results)
-        print('%.1f + %.1f + %.1f = %.1f' % (*params, float(results)))
+    results = executor.map(sim_run, *input_param)
+    print(list(results))
